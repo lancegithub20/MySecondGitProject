9ba2b336d38f22ecd9bb077c9a1b05541208c1c0
praveen <praveen.rajashekar@ef.com>
2020-07-17 17:45:28 +0530

    After Rebase push test

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("Rebase push test")
+        println("After Rebase push test")
 
     }
 }
