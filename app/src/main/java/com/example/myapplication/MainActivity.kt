507143a6388ad7b713329f02fc84014b247d2c9d
lancegithub20 <68284388+lancegithub20@users.noreply.github.com>
2020-07-17 17:38:17 +0530

    Revert "test push"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Test push")
+        println("Local branch")
+        println("Local branch2")
+        println("Local branch3")
     }
 }
