8d4e1a742629b0f83ec958dd6bc41b7bfac3620f
praveen <praveen.rajashekar@ef.com>
2020-07-17 17:52:36 +0530

    Feature branch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,6 +10,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("Rebase push test")
         println("After Rebase push test")
-
     }
 }
